34080d4f0a2c22ae97d8b8216bd41f7c733cb1c7
PARMARAJAYKUMAR <parmarajay0865@gmail.com>
2025-05-26 09:53:21 +0530

    Updated backend logic / bug fix / new feature

--- controller/resumeDownload.js
+++ controller/resumeDownload.js
@@ -1,72 +1,4 @@
 
-// const puppeteer = require("puppeteer");
-// const fs = require("fs");
-// const path = require("path");
-
-// exports.ResumeDownload = async (req, res) => {
-
-  
-//   try {
-//     const { htmlContent } = req.body;
-
-//     if (!htmlContent) {
-//       return res.status(400).json({ error: "❗HTML content is required!" });
-//     }
-
-//     const browser = await puppeteer.launch({
-//       headless: "new",
-//       args: ["--no-sandbox", "--disable-setuid-sandbox"],
-//     });
-
-//     const page = await browser.newPage();
-    
-
-//     await page.setContent(htmlContent, {
-//       waitUntil: ["domcontentloaded", "networkidle0"],
-//     });
-
-//     // ✅ Wait for fonts and icons to render fully
-//     await page.evaluateHandle("document.fonts.ready");
-
-//     // 🕐 Manual delay for icon rendering (older Puppeteer compatible)
-//     await new Promise((r) => setTimeout(r, 1000));
-
-//     await page.emulateMediaType("screen");
-
-//     const pdfPath = path.join(__dirname, "resume.pdf");
-
-//     const pdfBuffer = await page.pdf({
-//             format: "A4", // this auto sets height/width correctly in mm
-//             height: "1123px",
-//             width: "794px", // A4 size in pixels at 96 DPI
-//             path: pdfPath,
-//             printBackground: true,
-//             pageRanges: "1",
-//             preferCSSPageSize: true, 
-//             padding: { top: "0px", bottom: "0px", left: "0px", right: "0px" },
-//           });
-
-//     await browser.close();
-
-//       // Set headers for PDF download
-//     res.set({
-//       "Content-Type": "application/pdf",
-//       "Content-Disposition": 'attachment; filename="resume.pdf"',
-//       "Content-Length": pdfBuffer.length,
-//     });
-
-//     // Send the PDF buffer
-//     res.send(pdfBuffer);
-//     res.download(pdfPath, "resume.pdf", (err) => {
-//       if (err) {
-//         res.status(500).json({ error: "Failed to send PDF" });
-//       }
-//     });
-//   } catch (error) {
-//     res.status(500).json({ error: "Failed to generate PDF" });
-//   }
-// };
-
 const puppeteer = require("puppeteer");
 
 exports.ResumeDownload = async (req, res) => {
@@ -78,7 +10,7 @@ exports.ResumeDownload = async (req, res) => {
     }
 
     const browser = await puppeteer.launch({
-      headless: "new",
+      headless:true,
       args: ["--no-sandbox", "--disable-setuid-sandbox"],
     });
 
